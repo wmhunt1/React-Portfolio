dd6d474b7ecfc96402da4450658ea219a38184b8
Matt Hunt <wmatthunt@gmail.com>
2020-11-02 12:09:18 -0500

    now routes to pages

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Wrapper>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/portfolio" component={Portfolio} />
+          </Wrapper>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+  
+  export default App;
---
+++ react-portfolio/src/pages/Portfolio.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Portfolio(){
+    return(
+        <div>
+            <h1>Portfolio</h1>
+        </div>
+    )
+}
+export default Portfolio;
